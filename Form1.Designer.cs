940bb520c0a9a2ff9a4e354046126c7e65227b61
Reyna <reyna.anyer.nina@gmail.com>
2025-03-01 19:02:17 -0300

    enviado

---
+++ Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace calculadora
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            btnPonto = new Button();
+            btn0 = new Button();
+            btn1 = new Button();
+            btn2 = new Button();
+            btn3 = new Button();
+            btnIgual = new Button();
+            btn4 = new Button();
+            btn5 = new Button();
+            btn6 = new Button();
+            btn9 = new Button();
+            btn8 = new Button();
+            btn7 = new Button();
+            btnDivisao = new Button();
+            btnMultiplicacao = new Button();
+            btnSubstracao = new Button();
+            btnSoma = new Button();
+            btnLimpa = new Button();
+            textBox1 = new TextBox();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Purple;
+            panel1.Controls.Add(btnPonto);
+            panel1.Controls.Add(btn0);
+            panel1.Controls.Add(btn1);
+            panel1.Controls.Add(btn2);
+            panel1.Controls.Add(btn3);
+            panel1.Controls.Add(btnIgual);
+            panel1.Controls.Add(btn4);
+            panel1.Controls.Add(btn5);
+            panel1.Controls.Add(btn6);
+            panel1.Controls.Add(btn9);
+            panel1.Controls.Add(btn8);
+            panel1.Controls.Add(btn7);
+            panel1.Controls.Add(btnDivisao);
+            panel1.Controls.Add(btnMultiplicacao);
+            panel1.Controls.Add(btnSubstracao);
+            panel1.Controls.Add(btnSoma);
+            panel1.Controls.Add(btnLimpa);
+            panel1.Controls.Add(textBox1);
+            panel1.Location = new Point(6, 6);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(589, 566);
+            panel1.TabIndex = 0;
+            // 
+            // btnPonto
+            // 
+            btnPonto.Font = new Font("Ebrima", 24F, FontStyle.Bold);
+            btnPonto.Location = new Point(309, 450);
+            btnPonto.Name = "btnPonto";
+            btnPonto.Size = new Size(110, 98);
+            btnPonto.TabIndex = 20;
+            btnPonto.Text = ".";
+            btnPonto.UseVisualStyleBackColor = true;
+            btnPonto.Click += btnPonto_Click;
+            // 
+            // btn0
+            // 
+            btn0.Font = new Font("Ebrima", 24F, FontStyle.Bold);
+            btn0.Location = new Point(169, 450);
+            btn0.Name = "btn0";
+            btn0.Size = new Size(110, 98);
+            btn0.TabIndex = 19;
+            btn0.Text = "0";
+            btn0.UseVisualStyleBackColor = true;
+            btn0.Click += btn0_Click;
+            // 
+            // btn1
+            // 
+            btn1.Font = new Font("Ebrima", 24F, FontStyle.Bold);
+            btn1.Location = new Point(30, 450);
+            btn1.Name = "btn1";
+            btn1.Size = new Size(110, 98);
+            btn1.TabIndex = 18;
+            btn1.Text = "1";
+            btn1.UseVisualStyleBackColor = true;
+            btn1.Click += btn1_Click;
+            // 
+            // btn2
+            // 
+            btn2.Font = new Font("Ebrima", 24F, FontStyle.Bold);
+            btn2.Location = new Point(450, 339);
+            btn2.Name = "btn2";
+            btn2.Size = new Size(110, 98);
+            btn2.TabIndex = 17;
+            btn2.Text = "2";
+            btn2.UseVisualStyleBackColor = true;
+            btn2.Click += btn2_Click;
+            // 
+            // btn3
+            // 
+            btn3.Font = new Font("Ebrima", 24F, FontStyle.Bold);
+            btn3.Location = new Point(309, 339);
+            btn3.Name = "btn3";
+            btn3.Size = new Size(110, 98);
+            btn3.TabIndex = 16;
+            btn3.Text = "3";
+            btn3.UseVisualStyleBackColor = true;
+            btn3.Click += btn3_Click;
+            // 
+            // btnIgual
+            // 
+            btnIgual.Font = new Font("Ebrima", 24F, FontStyle.Bold);
+            btnIgual.Location = new Point(450, 450);
+            btnIgual.Name = "btnIgual";
+            btnIgual.Size = new Size(110, 98);
+            btnIgual.TabIndex = 15;
+            btnIgual.Text = "=";
+            btnIgual.UseVisualStyleBackColor = true;
+            btnIgual.Click += btnIgual_Click;
+            // 
+            // btn4
+            // 
+            btn4.Font = new Font("Ebrima", 24F, FontStyle.Bold);
+            btn4.Location = new Point(169, 339);
+            btn4.Name = "btn4";
+            btn4.Size = new Size(110, 98);
+            btn4.TabIndex = 13;
+            btn4.Text = "4";
+            btn4.UseVisualStyleBackColor = true;
+            btn4.Click += btn4_Click;
+            // 
+            // btn5
+            // 
+            btn5.Font = new Font("Ebrima", 24F, FontStyle.Bold);
+            btn5.Location = new Point(30, 339);
+            btn5.Name = "btn5";
+            btn5.Size = new Size(110, 98);
+            btn5.TabIndex = 12;
+            btn5.Text = "5";
+            btn5.UseVisualStyleBackColor = true;
+            btn5.Click += btn5_Click;
+            // 
+            // btn6
+            // 
+            btn6.Font = new Font("Ebrima", 24F, FontStyle.Bold);
+            btn6.Location = new Point(450, 230);
+            btn6.Name = "btn6";
+            btn6.Size = new Size(110, 98);
+            btn6.TabIndex = 11;
+            btn6.Text = "6";
+            btn6.UseVisualStyleBackColor = true;
+            btn6.Click += btn6_Click;
+            // 
+            // btn9
+            // 
+            btn9.Font = new Font("Ebrima", 24F, FontStyle.Bold);
+            btn9.Location = new Point(30, 230);
+            btn9.Name = "btn9";
+            btn9.Size = new Size(110, 98);
+            btn9.TabIndex = 8;
+            btn9.Text = "9";
+            btn9.UseVisualStyleBackColor = true;
+            btn9.Click += btn9_Click;
+            // 
+            // btn8
+            // 
+            btn8.Font = new Font("Ebrima", 24F, FontStyle.Bold);
+            btn8.Location = new Point(169, 230);
+            btn8.Name = "btn8";
+            btn8.Size = new Size(110, 98);
+            btn8.TabIndex = 7;
+            btn8.Text = "8";
+            btn8.UseVisualStyleBackColor = true;
+            btn8.Click += btn8_Click;
+            // 
+            // btn7
+            // 
+            btn7.Font = new Font("Ebrima", 24F, FontStyle.Bold);
+            btn7.Location = new Point(309, 230);
+            btn7.Name = "btn7";
+            btn7.Size = new Size(110, 98);
+            btn7.TabIndex = 6;
+            btn7.Text = "7";
+            btn7.UseVisualStyleBackColor = true;
+            btn7.Click += btn7_Click;
+            // 
+            // btnDivisao
+            // 
+            btnDivisao.Font = new Font("Ebrima", 24F, FontStyle.Bold);
+            btnDivisao.Location = new Point(461, 137);
+            btnDivisao.Name = "btnDivisao";
+            btnDivisao.Size = new Size(100, 80);
+            btnDivisao.TabIndex = 5;
+            btnDivisao.Text = "/";
+            btnDivisao.UseVisualStyleBackColor = true;
+            btnDivisao.Click += btnDivisao_Click;
+            // 
+            // btnMultiplicacao
+            // 
+            btnMultiplicacao.Font = new Font("Ebrima", 24F, FontStyle.Bold);
+            btnMultiplicacao.Location = new Point(353, 137);
+            btnMultiplicacao.Name = "btnMultiplicacao";
+            btnMultiplicacao.Size = new Size(100, 80);
+            btnMultiplicacao.TabIndex = 4;
+            btnMultiplicacao.Text = "*";
+            btnMultiplicacao.UseVisualStyleBackColor = true;
+            btnMultiplicacao.Click += btnMultiplicacao_Click;
+            // 
+            // btnSubstracao
+            // 
+            btnSubstracao.Font = new Font("Ebrima", 24F, FontStyle.Bold);
+            btnSubstracao.Location = new Point(243, 135);
+            btnSubstracao.Name = "btnSubstracao";
+            btnSubstracao.Size = new Size(100, 80);
+            btnSubstracao.TabIndex = 3;
+            btnSubstracao.Text = "-";
+            btnSubstracao.UseVisualStyleBackColor = true;
+            btnSubstracao.Click += btnSubstracao_Click;
+            // 
+            // btnSoma
+            // 
+            btnSoma.Font = new Font("Ebrima", 24F, FontStyle.Bold);
+            btnSoma.Location = new Point(137, 137);
+            btnSoma.Name = "btnSoma";
+            btnSoma.Size = new Size(100, 80);
+            btnSoma.TabIndex = 2;
+            btnSoma.Text = "+";
+            btnSoma.UseVisualStyleBackColor = true;
+            btnSoma.Click += btnSoma_Click;
+            // 
+            // btnLimpa
+            // 
+            btnLimpa.Font = new Font("Ebrima", 24F, FontStyle.Bold);
+            btnLimpa.Location = new Point(30, 135);
+            btnLimpa.Name = "btnLimpa";
+            btnLimpa.Size = new Size(100, 80);
+            btnLimpa.TabIndex = 1;
+            btnLimpa.Text = "C";
+            btnLimpa.UseVisualStyleBackColor = true;
+            btnLimpa.Click += btnLimpa_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Ebrima", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(30, 15);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(530, 103);
+            textBox1.TabIndex = 0;
+            textBox1.TextAlign = HorizontalAlignment.Right;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(64, 64, 64);
+            ClientSize = new Size(600, 578);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "CALCULADORA";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Button btnDivisao;
+        private Button btnMultiplicacao;
+        private Button btnSubstracao;
+        private Button btnSoma;
+        private Button btnLimpa;
+        private TextBox textBox1;
+        private Button btnPonto;
+        private Button btn0;
+        private Button btn1;
+        private Button btn2;
+        private Button btn3;
+        private Button btnIgual;
+        private Button btn4;
+        private Button btn5;
+        private Button btn6;
+        private Button btn9;
+        private Button btn8;
+        private Button btn7;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,135 @@
+namespace calculadora
+{
+    public partial class Form1 : Form
+    {
+        private double numero1, numero2, resultado;
+        private int operacao;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "0";
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "1";
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "2";
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "3";
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "4";
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "5";
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "6";
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "7";
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "8";
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "9";
+        }
+
+        private void btnSoma_Click(object sender, EventArgs e)
+        {
+            operacao = 1;
+            numero1 = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = "";
+        }
+
+        private void btnSubstracao_Click(object sender, EventArgs e)
+        {
+            operacao = 2;
+            numero1 = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = "";
+        }
+
+        private void btnMultiplicacao_Click(object sender, EventArgs e)
+        {
+            operacao = 3;
+            numero1 = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = "";
+        }
+
+        private void btnDivisao_Click(object sender, EventArgs e)
+        {
+            operacao = 4;
+            numero1 = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = "";
+        }
+
+        private void btnIgual_Click(object sender, EventArgs e)
+        {
+            numero2 = Convert.ToDouble(textBox1.Text);
+         
+            switch(operacao)
+            {
+                case 1:
+                    resultado = numero1 + numero2;
+                    break;
+                case 2:
+                    resultado = numero1 - numero2;
+                    break;
+                case 3:
+                    resultado = numero1 * numero2;
+                    break;
+                case 4:
+                    //resultado = numero1 / numero2;
+                    //break;
+                    if (numero2 != 0)
+                        resultado = numero1 / numero2;
+                    else
+                    {
+                        textBox1.Text = "ERRO!!!!!";
+                        return;
+                    }
+                    break;                  
+
+            }
+            textBox1.Text = resultado.ToString();
+
+
+
+        }
+
+        private void btnLimpa_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void btnPonto_Click(object sender, EventArgs e)
+        {
+            
+            textBox1.Text = textBox1.Text+".";
+        }
+    }
+}
